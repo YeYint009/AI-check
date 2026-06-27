2353f597e2cd1ea4cd718189812b77f41f54b131
YeYint009 <blackskym96@gmail.com>
2026-06-28 06:01:13 +0900

    adding next config

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,7 @@ const nextConfig: NextConfig = {
       bodySizeLimit: '10mb',
     },
   },  
+    serverExternalPackages: ['playwright-core', '@sparticuz/chromium'],
 };
 
 export default nextConfig;
